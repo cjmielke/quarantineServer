167043b3c65ce55bec3bda7268909d105cea1913
cosmo <clinton.mielke@gmail.com>
2020-04-08 00:30:49 +0000

    relearning old painful lessons ....

--- runserver.py
+++ runserver.py
@@ -20,20 +20,20 @@ class MyRequestHandler(WSGIRequestHandler):
 		super(MyRequestHandler, self).log_request(code, size)
 
 
+args = parser.parse_args()
+
+debug = False
+if args.debug: debug=True
+
+create_app(debug)
+print 'Debug state: ', app.debug
+
+if not debug:
+	from app.initializers.secrets import SENTRY_DSN
+	sentry = Sentry(app, dsn=SENTRY_DSN)
+
 
 def main():
-
-	args = parser.parse_args()
-
-	debug = False
-	if args.debug: debug=True
-
-	create_app(debug)
-	print 'Debug state: ', app.debug
-
-	if not debug:
-		from app.initializers.secrets import SENTRY_DSN
-		sentry = Sentry(app, dsn=SENTRY_DSN)
 
 	host='127.0.0.1'   # host of 0.0.0.0 makes debug server visible over network! Use sparingly
 
