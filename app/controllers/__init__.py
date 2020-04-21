b4ac92665838c5e06d124c940cf75d00a1327d90
cosmo <clinton.mielke@gmail.com>
2020-04-21 22:26:59 +0000

    ligand drawings in tables

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -49,7 +49,8 @@ def userLink(row):
 
 def smilesDrawing(row):
 	if row.smiles:
-		return '<img src="/static/ligandsvg/%s.svg">' % row.zincID
+		#return '<img src="/static/ligandsvg/%s.svg">' % row.zincID
+		return '<object data="/static/ligandsvg/%s.svg" type="image/svg+xml"></object>' % row.zincID
 	else: return ''
 
 
